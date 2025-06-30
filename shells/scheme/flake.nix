471adff0013953c5ebb5968a4f1e3f5b66398f9a
Robson Rodrigues <robsonrod@gmail.com>
2025-06-29 22:48:06 -0300

    Add development shells

---
+++ shells/scheme/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Scheme development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inputs = with pkgs; [ guile ];
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell
+          {
+            name = "scheme";
+            packages = inputs;
+          };
+      });
+}
