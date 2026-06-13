8c74c37dbe035ed022ea92964a7bf37a40b0fc95
Robson Rodrigues <robsonrod@gmail.com>
2026-06-13 18:31:20 -0300

    Update wofi

--- scripts/projects.nix
+++ scripts/projects.nix
@@ -8,13 +8,13 @@ pkgs.writeShellApplication {
   ];
 
   text = ''
-  projects=$(find "''$HOME/dev" -mindepth  2 -maxdepth 2 -type d -printf '%P\n')
-  [ -n "''${projects}" ] || exit 1
+    projects=$(find "''$HOME/dev" -mindepth  2 -maxdepth 2 -type d -printf '%P\n')
+    [ -n "''${projects}" ] || exit 1
 
-  selected=$(printf '%s\n' "''${projects}" | wofi --dmenu)
-  [ -n "''${selected}" ] || exit 2
+    selected=$(printf '%s\n' "''${projects}" | wofi -w 1 --dmenu -p "Find")
+    [ -n "''${selected}" ] || exit 2
 
-  project="''${HOME}/dev/''${selected}"
-  footclient --working-directory="''${project}"
+    project="''${HOME}/dev/''${selected}"
+    footclient --working-directory="''${project}"
   '';
 }
