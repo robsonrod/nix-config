51c3287bf1c3ace19410471f9821648b9b6aeee6
Robson Rodrigues <robsonrod@gmail.com>
2026-06-19 14:22:31 -0300

    Update config

--- configuration/modules/boot.nix
+++ configuration/modules/boot.nix
@@ -14,6 +14,18 @@
         devices = [ "nodev" ];
         configurationLimit = 5;
         gfxmodeEfi = "1920x1080x32";
+
+        theme = pkgs.stdenv.mkDerivation {
+          pname = "nix-gruv-grub";
+          version = "1";
+          src = pkgs.fetchFromGitHub {
+            owner = "robsonrod";
+            repo = "nix-gruv-grub";
+            rev = "269507de98ecd4fd9c57aa06bf5d8132d6949a06";
+            hash = "sha256-UEPZxyT09Z0PiOka/Dh4m8VvqF4l+01eZVbRkPJduDk=";
+          };
+          installPhase = "cp -r tartarus/ $out";
+        };
       };
     };
   };
